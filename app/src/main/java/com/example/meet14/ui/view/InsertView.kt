6c54f7f4e243584f155b7859e77d25826d07960d
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-11 15:22:13 +0700

    Menambahkan observasi  perubahan untuk snackbar dan navigasi

--- app/src/main/java/com/example/meet14/ui/view/InsertView.kt
+++ app/src/main/java/com/example/meet14/ui/view/InsertView.kt
@@ -1,8 +1,13 @@
 package com.example.meet14.ui.view
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.meet14.ui.viewModel.FormState
 import com.example.meet14.ui.viewModel.InsertViewModel
 import com.example.meet14.ui.viewModel.PenyediaViewModel
 
@@ -15,4 +20,16 @@ fun InsertMhsView(
 ){
     val uiState = viewModel.uiState
     val uiEvent = viewModel.uiEvent
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState) {
+        when (uiState){
+            is FormState.Success -> {
+                println(
+                    "InsertMhsView: uiState is FormState.Success,navigate to home" + uiState.message
+                )
+            }
+        }
+    }
 }
