88ee17fc0c433569b9f49f8970908f8a4b3f6e85
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-11 14:59:22 +0700

    membuat data class variabel yang untuk menyimpan data input form

--- app/src/main/java/com/example/meet14/ui/viewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/meet14/ui/viewModel/InsertViewModel.kt
@@ -81,7 +81,19 @@ data class FormErrorState(
     val alamat: String? = null,
     val kelas: String? = null,
     val angkatan: String? = null
-){
-
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
 }
+// data class variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
 
