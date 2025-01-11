0319edb499b1b62e63d34bc718ca572e572b82b5
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-11 15:15:13 +0700

    membuat tampilan untuk tambah data mahasiswa dan mengatur aksi kembali atau pindah layar

---
+++ app/src/main/java/com/example/meet14/ui/view/InsertView.kt
@@ -0,0 +1,17 @@
+package com.example.meet14.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.meet14.ui.viewModel.InsertViewModel
+import com.example.meet14.ui.viewModel.PenyediaViewModel
+
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
