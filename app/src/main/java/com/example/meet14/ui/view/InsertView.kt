480d66f536bfc449e362214fdd04c85a16b13536
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-11 15:31:20 +0700

    Membuat Fungsi InsertBodyMhs

--- app/src/main/java/com/example/meet14/ui/view/InsertView.kt
+++ app/src/main/java/com/example/meet14/ui/view/InsertView.kt
@@ -1,14 +1,25 @@
 package com.example.meet14.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.meet14.ui.viewModel.FormState
+import com.example.meet14.ui.viewModel.InsertUiState
 import com.example.meet14.ui.viewModel.InsertViewModel
+import com.example.meet14.ui.viewModel.MahasiswaEvent
 import com.example.meet14.ui.viewModel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
@@ -34,6 +45,51 @@ fun InsertMhsView(
                     snackbarHostState.showSnackbar(uiState.message)
                 }
             }
+            else -> Unit
         }
     }
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState)},
+        topBar = {
+            TopAppBar(
+                title = { Text("Tambah Mahasiswa")},
+                navigationIcon = {
+                    Button(onClick = onBack) {
+                        Text("Back")
+                    }
+                }
+            )
+        }
+    ){ padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+            InsertBodyMhs(
+                uiState = uiEvent, homeUiState = uiState, onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)
+                },
+                onClick = {
+                    if (viewModel.validateFields()) {
+                        viewModel.insertMhs()
+                        //onNavigate()
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun InsertBodyMhs (
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent ) -> Unit,
+    uiState: InsertUiState,
+    onClick: () -> Unit,
+    homeUiState: FormState
+){
+
 }
