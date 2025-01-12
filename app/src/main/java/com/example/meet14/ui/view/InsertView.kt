79c34f902374668c0d7f7b280394e3a0085e9dfb
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-12 13:05:49 +0700

    menampilkan tampilan untuk menambahkan data mahasiswa

--- app/src/main/java/com/example/meet14/ui/view/InsertView.kt
+++ app/src/main/java/com/example/meet14/ui/view/InsertView.kt
@@ -1,35 +1,13 @@
 package com.example.meet14.ui.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.meet14.ui.viewModel.FormErrorState
-import com.example.meet14.ui.viewModel.FormState
-import com.example.meet14.ui.viewModel.InsertUiState
 import com.example.meet14.ui.viewModel.InsertViewModel
-import com.example.meet14.ui.viewModel.MahasiswaEvent
 import com.example.meet14.ui.viewModel.PenyediaViewModel
-import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InsertMhsView(
     onBack: () -> Unit,
@@ -37,104 +15,3 @@ fun InsertMhsView(
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-    val uiState = viewModel.uiState
-    val uiEvent = viewModel.uiEvent
-    val snackbarHostState = remember { SnackbarHostState() }
-    val coroutineScope = rememberCoroutineScope()
-
-    LaunchedEffect(uiState) {
-        when (uiState){
-            is FormState.Success -> {
-                println(
-                    "InsertMhsView: uiState is FormState.Success,navigate to home" + uiState.message
-                )
-                coroutineScope.launch {
-                    snackbarHostState.showSnackbar(uiState.message)
-                }
-            }
-            else -> Unit
-        }
-    }
-    Scaffold(
-        modifier = modifier,
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState)},
-        topBar = {
-            TopAppBar(
-                title = { Text("Tambah Mahasiswa")},
-                navigationIcon = {
-                    Button(onClick = onBack) {
-                        Text("Back")
-                    }
-                }
-            )
-        }
-    ){ padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .padding(16.dp)
-        ){
-            InsertBodyMhs(
-                uiState = uiEvent, homeUiState = uiState, onValueChange = { updatedEvent ->
-                    viewModel.updateState(updatedEvent)
-                },
-                onClick = {
-                    if (viewModel.validateFields()) {
-                        viewModel.insertMhs()
-                        //onNavigate()
-                    }
-                }
-            )
-        }
-    }
-}
-
-@Composable
-fun InsertBodyMhs (
-    modifier: Modifier = Modifier,
-    onValueChange: (MahasiswaEvent ) -> Unit,
-    uiState: InsertUiState,
-    onClick: () -> Unit,
-    homeUiState: FormState
-){
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        FormMahasiswa(
-            mahasiswaEvent = uiState.insertUiEvent,
-            onValueChange = onValueChange,
-            errorState = uiState.isEntryValid,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Button(
-            onClick = onClick,
-            modifier = Modifier.fillMaxWidth(),
-            enabled = homeUiState !is FormState.Loading,
-        ) {
-            if (homeUiState is FormState.Loading) {
-                CircularProgressIndicator(
-                    color = Color.White,
-                    modifier = Modifier
-                        .size(20.dp)
-                        .padding(end = 8.dp)
-                )
-                Text("Loading...")
-            } else {
-                Text("Add")
-            }
-        }
-    }
-}
-
-@Composable
-fun FormMahasiswa(
-    mahasiswaEvent: MahasiswaEvent = MahasiswaEvent(),
-    onValueChange: (MahasiswaEvent) -> Unit,
-    errorState: FormErrorState = FormErrorState(),
-    modifier: Modifier = Modifier
-) {
-
-}
