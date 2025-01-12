3bde969210e435345805a55ffb98485f04d20897
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-12 12:22:18 +0700

    Perbaikan typo pada getMahasiswaById menjadi getMahasiswaByNim

--- app/src/main/java/com/example/meet14/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/meet14/repository/MahasiswaRepository.kt
@@ -4,9 +4,10 @@ import com.example.meet14.model.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 interface MahasiswaRepository {
-    suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
-    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
-    suspend fun updateMahasiswa(nim:String,mahasiswa: Mahasiswa)
-    suspend fun deleteMahasiswa(nim:String)
-    suspend fun getMahasiswaByNIM(nim:String): Flow<Mahasiswa>
+   suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
+   suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+   suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa>
+   suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+   suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
 }
