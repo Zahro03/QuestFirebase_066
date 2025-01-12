c9e617a357a79173a5fb2bfe641bb915ed862513
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-12 12:41:27 +0700

    Membuat class NetworkRepositoryMhs

---
+++ app/src/main/java/com/example/meet14/repository/NetworkRepositoryMhs.kt
@@ -0,0 +1,8 @@
+package com.example.meet14.repository
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class NetworkRepositoryMhs(
+    private val firestore: FirebaseFirestore
+) : MahasiswaRepository {
+}
