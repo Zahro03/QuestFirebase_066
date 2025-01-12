2d4c591fba366bb839bd13817863f074880659ef
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-12 12:51:03 +0700

    menghapus data mahasiswa di Firestore berdasarkan NIM

--- app/src/main/java/com/example/meet14/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/meet14/repository/NetworkRepositoryMhs.kt
@@ -50,4 +50,16 @@ class NetworkRepositoryMhs(
         }
     }
 
+    override suspend fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa:${e.message}")
+        }
+    }
+
+
 
