b4126458acbeca1b1b4613c8e3e6bc9ee5652868
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-12 13:17:19 +0700

    perbaikan typo

--- app/src/main/java/com/example/meet14/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/meet14/ui/viewModel/PenyediaViewModel.kt
@@ -6,10 +6,12 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.meet14.MahasiswaApplications
+import com.example.meet14.model.Mahasiswa
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(MahasiswaApplications().container.mahasiswaRepository)}
+        initializer { InsertViewModel(MahasiswaApplications().container.mahasiswaRepository) }
     }
     fun CreationExtras.MahasiswaApplications(): MahasiswaApplications =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
