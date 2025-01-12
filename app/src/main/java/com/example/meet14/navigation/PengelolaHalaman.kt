591ecb21f2f89bcb80b90e213d8f0e534a008373
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-12 12:36:12 +0700

    Membuat sistem navigasi antar halaman agar aplikasi bisa berpindah-pindah tampilan

---
+++ app/src/main/java/com/example/meet14/navigation/PengelolaHalaman.kt
@@ -0,0 +1,39 @@
+package com.example.meet14.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.meet14.ui.view.HomeScreen
+import com.example.meet14.ui.view.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToltemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
