4a0ff3d3bce0c2ccdc01843b0cdb4e0da89cca44
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-15 12:57:47 +0700

    Membuat Class ViewModel untuk mengelola data dan status UI untuk DetailMhs

--- app/src/main/java/com/example/meet14/ui/viewModel/DetailViewModel.kt
+++ app/src/main/java/com/example/meet14/ui/viewModel/DetailViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.meet14.ui.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.meet14.model.Mahasiswa
+import com.example.meet14.repository.MahasiswaRepository
 
 // Kelas sealed untuk merepresentasikan berbagai status UI pada DetailMhs
 sealed class DetailMhsUiState {
@@ -8,3 +14,16 @@ sealed class DetailMhsUiState {
     object Error : DetailMhsUiState()
     object Loading : DetailMhsUiState()
 }
+
+// ViewModel untuk mengelola data dan status UI pada DetailMhs
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+) : ViewModel() {
+    private val nim: String = checkNotNull(savedStateHandle["nim"])
+    var detailMhsUiState: DetailMhsUiState by mutableStateOf(DetailMhsUiState.Loading)
+        private set
+
+    init {
+        getMahasiswaByNim()
+    }
