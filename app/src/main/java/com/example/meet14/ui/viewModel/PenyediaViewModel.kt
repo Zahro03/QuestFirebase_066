69c947df7b9191a6770405f90d785a990bdd51e3
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-15 13:05:25 +0700

    Menambahkan Inisialisasi untuk DetailViewModel

--- app/src/main/java/com/example/meet14/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/meet14/ui/viewModel/PenyediaViewModel.kt
@@ -1,17 +1,16 @@
 package com.example.meet14.ui.viewModel
 
-import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.meet14.MahasiswaApplications
-import com.example.meet14.model.Mahasiswa
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(MahasiswaApplications().container.mahasiswaRepository)}
         initializer { InsertViewModel(MahasiswaApplications().container.mahasiswaRepository) }
+        initializer { DetailViewModel(MahasiswaApplications().container.mahasiswaRepository) }
     }
     fun CreationExtras.MahasiswaApplications(): MahasiswaApplications =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
