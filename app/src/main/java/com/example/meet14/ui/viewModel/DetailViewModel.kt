97ae78ead7ff161e3f218e0c90a7af0b825e37e1
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-15 12:58:47 +0700

    Menambahkan fungsi untuk mengambil data mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/meet14/ui/viewModel/DetailViewModel.kt
+++ app/src/main/java/com/example/meet14/ui/viewModel/DetailViewModel.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.meet14.model.Mahasiswa
 import com.example.meet14.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 // Kelas sealed untuk merepresentasikan berbagai status UI pada DetailMhs
 sealed class DetailMhsUiState {
@@ -27,3 +29,17 @@ class DetailViewModel(
     init {
         getMahasiswaByNim()
     }
+
+    // Fungsi untuk mengambil data mahasiswa berdasarkan NIM
+    fun getMahasiswaByNim() {
+        viewModelScope.launch {
+            detailMhsUiState = DetailMhsUiState.Loading
+            detailMhsUiState = try {
+                DetailMhsUiState.Success(mahasiswaRepository.getMahasiswaByNim(nim))
+            } catch (e: Exception) {
+                DetailMhsUiState.Error
+            }
+        }
+
+    }
+}
